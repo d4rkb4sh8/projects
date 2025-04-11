89196bbb744f675910fd89a7a03ff711a879a64a
d4rkb4sh8 <d4rkb4sh8@gmail.com>
2025-04-11 19:44:32 -0400

    updated

---
+++ python/indentation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
---
+++ python/variables_and_types.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# NUMBERS
+myint = 7
+print(myint)
+
+
+# To define a floating point number
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+
+# STRINGS
+mystring = "hello"
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+# SIMPLE OPERATORS
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+# ASSIGNMENTS
+a, b = 3, 4
+print(a, b)
